7d846b1043270b8278e36716370895a15a7db342
Stepan1328 <stepansamsonov1337@gmail.com>
2021-03-04 13:11:25 +0300

    "Fixed parallel game launch errors"

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
-import start "github.com/Stepan1328/game-test-bot/start_bot"
+import (
+	"bufio"
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"os"
+)
 
 func main() {
-	bot, updates := start.StartBot()
+	bot, updates := startBot()
 
-	start.ActionsWithUpdates(updates, bot)
+	ActionsWithUpdates(updates, bot)
+}
+
+func startBot() (*tgbotapi.BotAPI, tgbotapi.UpdatesChannel) {
+	botToken := takeBotToken()
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	u := tgbotapi.NewUpdate(0)
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		panic("Failed to initialize bot: " + err.Error())
+	}
+
+	log.Println("The bot is running")
+
+	return bot, updates
+}
+
+func takeBotToken() string {
+	var botToken string
+
+	file, err := os.Open("./botToken.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		botToken = s.Text()
+	}
+
+	return botToken
 }
