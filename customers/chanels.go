14cd0f74aa40ddc88bfa0e19a83fd7d51a7e3e96
Stepan1328 <stepansamsonov1337@gmail.com>
2021-03-14 23:29:59 +0300

    "added channels and battle map"

--- customers/chanels.go
+++ customers/chanels.go
@@ -4,7 +4,9 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 
 var (
 	TranslateUpdate = make(chan tgbotapi.CallbackQuery)
+	TranslateBattle = make(chan tgbotapi.CallbackQuery)
 	StopChannel     = make(chan tgbotapi.Message)
 	Players         = make(map[int]*UsersStatistic)
+	Battles         = make(map[int]*BattleStatistic)
 	OccupiedSells   = make([]int, 0)
 )
